4f87ab766368b957b58bd5df6e82ef86bcaae1cb
TrafficConeGod <TrafficConeGod@github.com>
2022-03-12 12:36:22 -0500

    template read_file

--- src/gl/io/read_file.hpp
+++ src/gl/io/read_file.hpp
@@ -2,7 +2,17 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 #include <string>
+#include <fstream>
 
 namespace gl::io {
-    std::string read_file(const fs::path& path);
+    template<typename T = char>
+    std::basic_string<T> read_file(const fs::path& path) {
+        std::ifstream file(path);
+        if (!file.is_open()) {
+            throw std::runtime_error("Could not open file: " + path.string());
+        }
+        std::basic_stringstream<T> buffer;
+        buffer << file.rdbuf();
+        return buffer.str();
+    }
 }
