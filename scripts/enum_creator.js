a19988293a8616777e489c94337b1157e5481948
TrafficConeGod <TrafficConeGod@github.com>
2022-03-13 18:27:06 -0400

    use GLenums

--- scripts/enum_creator.js
+++ scripts/enum_creator.js
@@ -2,9 +2,9 @@ const src = require("fs").readFileSync("scripts/source.c").toString();
 
 const lines = src.split(/\r?\n/);
 for (const line of lines) {
-    const sub = line.slice(11, line.length);
+    const sub = line.slice(8, line.length);
     const temp = sub.split(/\s+/);
-    const name = temp[0];
-    const value = temp[1];
+    const name = temp[0].slice(3, temp[0].length);
+    const value = temp[0];
     console.log(`${name} = ${value},`);
 }
--- src/gl/enums/clear_frame_option.hpp
+++ src/gl/enums/clear_frame_option.hpp
@@ -3,8 +3,8 @@
 
 namespace gl::enums {
     enum class clear_frame_option : gl::bit_field {
-        COLOR = 0x00004000,
-        DEPTH = 0x00000100,
-        STENCIL = 0x00000400,
+        COLOR = GL_COLOR_BUFFER_BIT,
+        DEPTH = GL_DEPTH_BUFFER_BIT,
+        STENCIL = GL_STENCIL_BUFFER_BIT
     };
 }
